0c74b179307db91dfd18426259b46133afada559
user <aew11111d@gmail.com>
2020-03-27 17:44:12 +0200

    commit

---
+++ public/js/app.js
@@ -0,0 +1,25 @@
+console.log('Client side javascript file is loaded!')
+
+const form = document.forms[0];
+
+const btn_search = document.getElementById('btn_search');
+btn_search.addEventListener('click',(e) => {
+    e.preventDefault();
+    const location = form.elements.input_location.value;
+    fetch('http://localhost:3000/weather?address=' + location).then((response) => {
+    response.json().then((data) => {
+        if (data.error) {
+            console.log(data.error)
+        } else {
+            console.log(data.location)
+            console.log(data.forecast)
+            document.querySelector('.forecast-data').innerHTML = data.location + '<br>' + data.forecast;
+        }
+    })
+})
+});
+
+
+
+
+
